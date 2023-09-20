44e7172427349d22c45f41e92c73e63025390b85
2020018004 <smha0101@maver.com>
2023-09-20 11:17:51 +0900

    final

--- src/main/java/org/example/sse/CorsConfig.java
+++ src/main/java/org/example/sse/CorsConfig.java
@@ -3,12 +3,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/v1/sse/broadcast") // 허용할 엔드포인트를 지정
-                .allowedOrigins("http://3.36.130.233:8080") // 허용할 오리진을 지정
-                .allowedMethods("POST"); // 허용할 HTTP 메서드를 지정
+        registry.addMapping("/v1/sse/subscribe/**") // 허용할 엔드포인트를 지정
+//                .allowedOrigins("http://3.36.130.233:8080") // 허용할 오리진을 지정
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET"); // 허용할 HTTP 메서드를 지정
     }
 }
---
+++ src/main/java/org/example/sse/sseSseController.java
@@ -0,0 +1,30 @@
+//package org.example.sse;
+//
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.Writer;
+//
+//@RestController
+//@RequestMapping("/sse")
+//public class sseSseController {
+//    @GetMapping
+//    public void sse(final HttpServletResponse response) throws IOException, InterruptedException {
+//        response.setContentType("text/event-stream");
+//        response.setCharacterEncoding("UTF-8");
+//
+//        Writer writer = response.getWriter();
+//
+//        for (int i = 0; i < 20; i++) {
+//            writer.write("data: " + System.currentTimeMillis() + "\n\n");
+//            writer.flush(); // 꼭 flush 해주어야 한다.
+//            Thread.sleep(1000);
+//        }
+//
+//        writer.close();
+//    }
+//}
